e8c51b0dc33095aed1a8be4169219e16a33f20c7
Noibauer <pedro.honoibauer@aluno.edu.es.gov.br>
2025-09-03 10:42:12 -0300

    Create index.php

---
+++ Aula 01 - 3º Trimestre/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title> Aula 01 PHP </title>
+    </head>
+    <body>
+        <?php
+            echo "Olá, Mundo!";
+            echo "Pedro Noibauer";
+        ?>
+    </body>
+</html> 
