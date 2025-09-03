84650a5891072b388bc41eb512812553ca38873a
Noibauer <pedro.honoibauer@aluno.edu.es.gov.br>
2025-09-03 11:01:52 -0300

    Create index.php

---
+++ Aula 02 - 3° Trimestre/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+    </head>
+        <title> Aula 02 PHP </title>
+    <body>
+        <?php
+        $nome = "Pedro Noibauer"
+
+        echo "Meu nome é: " .$nome;
+        ?>
+    </body>
+</html>
