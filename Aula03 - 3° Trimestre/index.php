d6054c0b248cc664c329f9e05b2ea035d857e184
Noibauer <pedro.honoibauer@aluno.edu.es.gov.br>
2025-09-09 11:09:18 -0300

    Create index.php

---
+++ Aula03 - 3° Trimestre/index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+    </head>
+        <title> Aula 03 PHP </title>
+    <body>
+        <?php
+        $nome = "Pedro Noibauer";
+
+        $N1 = 10;
+        $N2 = 2;
+        $soma = $N1 + $N2;
+
+        echo "Meu nome é " .$nome;
+        echo "<br> Meu nome é $nome";
+
+        echo"<br> <br> A soma de $N1 e $N2 é igual a $soma.";
+
+        ?>
+    </body>
+</html>
