b8db42ddfd873e2d310a241f8ab9e204dede27d3
Noibauer <pedro.honoibauer@aluno.edu.es.gov.br>
2025-09-10 10:54:52 -0300

    Update index.php

--- Aula 01 - 3º Trimestre/index.php
+++ Aula 01 - 3º Trimestre/index.php
@@ -1,11 +1,28 @@
 <html>
     <head>
-        <title> Aula 01 PHP </title>
     </head>
+        <title> Aula 03 PHP </title>
     <body>
         <?php
-            echo "Olá, Mundo!";
-            echo "Pedro Noibauer";
+        $nome = "Pedro Noibauer";
+
+        $N1 = 10;
+        $N2 = 2;
+        $soma = $N1 + $N2;
+
+        echo "Meu nome é " .$nome;
+        echo "<br> Meu nome é $nome";
+
+        echo"<br> <br> A soma de $N1 e $N2 é igual a $soma.";
+
+        echo"<br><br> O resultado da subtração de $N1 e $N2 é igual a: " .($N1-$N2) . ".";
+
+        echo"<br><br> O resultado da multiplicação de $N1 e $N2 é igual a: " .($N1*$N2) .".";
+
+        echo"<br><br> O resultado da divisão de $N1 e $N2 é igual a: " .($N1/$N2) .".";
+
+        echo"<br><br> O resultado da multiplicação de $N1 e $N2 é igual a: " .($N1%$N2) .".";
+
         ?>
     </body>
-</html> 
+</html>
