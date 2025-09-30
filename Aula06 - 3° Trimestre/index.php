1bb5b0793041ecacfc9c0b2f9fec7026e8c39d8f
Noibauer <pedro.honoibauer@aluno.edu.es.gov.br>
2025-09-30 11:07:55 -0300

    Create index.php

---
+++ Aula06 - 3° Trimestre/index.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title> Aula06 </title>
+    </head>
+    <body>
+        <h3> Operadores de Atribuição </h3>
+        <hr>
+
+        <?php
+            $a=2;
+            $b=$a+5;
+            echo"A é igual a $a e B é igual a $b";
+            $a=$a+3;
+            echo"<br>Agora A é igual a $a";
+
+            $b+=8;
+            echo"<br>Agora B é igual a $b";
+
+            $b-=5;
+            echo"<br>Agora B é igual a $b";
+
+
+        ?>
+
+    </body>
+</html>
